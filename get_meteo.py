b7f5251255d3f6f4350723629ade5b2df236ff30
WilliamBSITT <william.sitt@epitech.eu>
2024-08-02 18:01:44 +0200

    <+> ADD all the wheater data are in a json, now i need to extract the information

--- get_meteo.py
+++ get_meteo.py
@@ -2,6 +2,8 @@ import requests
 import os
 from dotenv import load_dotenv
 
+# For this projet I use the free api of openWeather
+Base_URL = "https://api.openweathermap.org/data/2.5/weather?"
 
 def convert_kelvin_to_celisus(kelvin):
     celsius = kelvin - 273.15
@@ -14,7 +16,16 @@ def convert_kelvin_to_fahrenheit(kelvin):
 def main():
     load_dotenv()
     city = os.getenv("CITY")
-    print(city)
+    api_key = os.getenv("API_KEY")
+    if city == None or api_key == None:
+        print(f"Error, it seems there is an error when accessing the data from dotenv.")
+    url = Base_URL + "appid=" + str(api_key) + "&q=" + str(city)
+    result = requests.get(url)
+    if result.status_code == 200:
+        wheater_data = result.json()
+        print(wheater_data)
+    else:
+        print(f"Error {result.status_code}")
 
     
 if __name__ == "__main__":
