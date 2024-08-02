4df4858fc0584885d7913e2a7177227df41a7b7c
WilliamBSITT <william.sitt@epitech.eu>
2024-08-02 20:15:13 +0200

    <+> ADD translate of the wheater in french

--- get_meteo.py
+++ get_meteo.py
@@ -4,6 +4,15 @@ from dotenv import load_dotenv
 
 # For this projet I use the free api of openWeather
 Base_URL = "https://api.openweathermap.org/data/2.5/weather?"
+meteo_type = {
+                "broken clouds": "éclaircies", 
+                "clear sky": "ensoleillé", 
+                "scattered clouds": "Ciel couvert", 
+                "snow": "Chute de neige", 
+                "overcast clouds": "Nuageux", 
+                "moderate rain" : "Pluie modéré", 
+                "light rain" : "Pluie légère"
+            }
 
 def convert_kelvin_to_celisus(kelvin):
     celsius = kelvin - 273.15
@@ -16,12 +25,21 @@ def convert_kelvin_to_fahrenheit(kelvin):
 def display(data, city):
     temp_kelvin = data["main"]["temp"]
     temp_feel_kelvin = data["main"]["feels_like"]
-    temp_fahrenheit = convert_kelvin_to_fahrenheit(temp_kelvin)
-    temp_feel_fahrenheit = convert_kelvin_to_fahrenheit(temp_feel_kelvin)
     humdity = data["main"]["humidity"]
     meteo = data["weather"][0]["description"]
-    print(f"The current weather in {city} is :\nA temperature of {temp_fahrenheit:.2f}°F with a fell of {temp_feel_fahrenheit:.2f}.")
-    print(f"The humidity is about {humdity}% and the wheater is {meteo}.")
+    if data["sys"]["country"] == "FR":
+        for i, j in meteo_type.items():
+            if i == meteo:
+                translate_weather = j
+        temp_celsius = convert_kelvin_to_celisus(temp_kelvin)
+        temp_feel_celsius = convert_kelvin_to_celisus(temp_feel_kelvin)
+        print(f"La météo actuelle à {city} est de :\nAvec un température de {temp_celsius:.2f}°C avec un ressentie de {temp_feel_celsius}.")
+        print(f"L'humidité est d'environ {humdity}% et la météo est {translate_weather}.")
+    else :
+        temp_fahrenheit = convert_kelvin_to_fahrenheit(temp_kelvin)
+        temp_feel_fahrenheit = convert_kelvin_to_fahrenheit(temp_feel_kelvin)
+        print(f"The current weather in {city} is :\nA temperature of {temp_fahrenheit:.2f}°F with a fell of {temp_feel_fahrenheit:.2f}.")
+        print(f"The humidity is about {humdity}% and the wheater is {meteo}.")
 
 def main():
     load_dotenv()
